576934a9753d43a1214206264e0280cb8ae47749
baicorp <bagusatoki@gmail.com>
2024-06-05 12:53:55 +0700

    comment research page fetch

--- app/research/page.tsx
+++ app/research/page.tsx
@@ -10,40 +10,41 @@ import {
 import { BASE_URL } from "@/constant/constant";
 
 export default async function Page() {
-  const res = await fetch(`${BASE_URL}/api/media`, {
-    // Use http://
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      context: {
-        client: {
-          clientName: "ANDROID_MUSIC",
-          clientVersion: "5.26.1",
-          androidSdkVersion: 30,
-        },
-        thirdParty: {
-          embedUrl: "https://music.youtube.com",
-        },
-      },
-      attestationRequest: {
-        omitBotguardData: true,
-      },
-      racyCheckOk: true,
-      contentCheckOk: true,
-      videoId: "SfLfO8vBQk4", // Replace with actual video ID
-      params: "2AMBCgIQBg",
-    }),
-  });
+  // const res = await fetch(`${BASE_URL}/api/media`, {
+  //   // Use http://
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({
+  //     context: {
+  //       client: {
+  //         clientName: "ANDROID_MUSIC",
+  //         clientVersion: "5.26.1",
+  //         androidSdkVersion: 30,
+  //       },
+  //       thirdParty: {
+  //         embedUrl: "https://music.youtube.com",
+  //       },
+  //     },
+  //     attestationRequest: {
+  //       omitBotguardData: true,
+  //     },
+  //     racyCheckOk: true,
+  //     contentCheckOk: true,
+  //     videoId: "SfLfO8vBQk4", // Replace with actual video ID
+  //     params: "2AMBCgIQBg",
+  //   }),
+  // });
 
-  const data = await res.json();
+  // const data = await res.json();
 
   return (
     <>
-      <pre>
+      {/* <pre>
         <code>{JSON.stringify(data, null, 2)}</code>
-      </pre>
+      </pre> */}
+      <p>hello world</p>
     </>
   );
 }
