2ba194bae89c7cbabd7ce294e12a0059a29c127f
baicorp <bagusatoki@gmail.com>
2024-06-22 17:48:47 +0700

    update api

--- app/api/stream/piped/route.js
+++ app/api/stream/piped/route.js
@@ -1,6 +1,4 @@
 import { NextResponse } from "next/server";
-import { Clients, Innertube } from "youtubei.js/web";
-
 export async function GET(request) {
   const url = new URL(request.url);
   const videoId = url.searchParams.get("videoId");
@@ -17,20 +15,4 @@ export async function GET(request) {
   if (!data) return NextResponse.json({ error: response.status });
   const stream = data?.audioStreams?.filter((data) => data?.itag === 251);
   return NextResponse.json(stream[0]?.url);
-}
-
-export async function POST(request) {
-  const url = new URL(request.url);
-  const videoId = url.searchParams.get("videoId");
-
-  if (!videoId) return NextResponse.json({ error: "no id given" });
-
-  console.log(videoId);
-
-  const yt = await Innertube.create();
-  const info = await yt.getInfo(videoId);
-
-  const urlStream = info.streaming_data?.formats[0].decipher(yt.session.player);
-
-  return NextResponse.json(urlStream);
 }
